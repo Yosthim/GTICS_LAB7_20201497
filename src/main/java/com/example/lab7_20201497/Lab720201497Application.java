853b9581c0f4c578d6576d75e0f7e7a16e2fdc2d
Yosthim <a20201497@pucp.edu.pe>
2024-05-30 19:41:41 -0500

    proyecto base

---
+++ src/main/java/com/example/lab7_20201497/Lab720201497Application.java
@@ -0,0 +1,13 @@
+package com.example.lab7_20201497;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab720201497Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab720201497Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab7_20201497/Lab720201497ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab7_20201497;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab720201497ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
